ed16044e84a475b10705a36b7ecc0767b41a6f60
ilatif <ilatif.bwp@gmail.com>
2014-06-23 13:09:20 +0500

    Fixed #1

--- image_zoomer.js
+++ image_zoomer.js
@@ -1,7 +1,7 @@
 (function($) {
 
   var zoom_context, zoom_canvas  = "<canvas id='zoom_canvas' style='position: absolute;'></canvas>";
-  var zoom_canvas_container_name = "#zoom_canvas_container", zoom_canvas_name = "#zoom_canvas";
+  var zoom_canvas_container_name = "#zoom_canvas_container", zoom_canvas_name = "#zoom_canvas", temp_image_canvas_context;
 
   $.fn.image_zoomer = function(options) {
 
@@ -19,8 +19,9 @@
       if ($(zoom_canvas_container_name).length == 0) {
         $(this).wrap("<span id='zoom_canvas_container'></span>");
         var zoom_canvas_container = $(zoom_canvas_container_name);
+        var temp_image_canvas     = _insert_temp_canvas(zoom_canvas_container, image);
         zoom_canvas_container.mousemove(function(e) {
-          zoom_image_mousemove(zoom_canvas_container, e, options, image);
+          zoom_image_mousemove(zoom_canvas_container, e, options, image, temp_image_canvas);
           return false;
         }).mouseleave(zoom_image_mouseleave);
       }
@@ -28,24 +29,24 @@
     return this;
   }
 
-  function zoom_image_mousemove(elem, e, options, image) {
+  function zoom_image_mousemove(elem, e, options, image, temp_image_canvas) {
     if ($(zoom_canvas_name).length == 0) {
       $(elem).append(zoom_canvas);
       $(zoom_canvas_name).attr("width", options.width).attr("height", options.height).css({"border-radius": "50%", "-moz-border-radius": "50%", "-webkit-border-radius": "50%"});
       zoom_context = $(zoom_canvas_name).get(0).getContext('2d');
       zoom_context.scale(options.scale, options.scale);
     }
-    _zoom_image(e, options, image);
+    _zoom_image(e, options, image, temp_image_canvas);
   }
 
   function zoom_image_mouseleave(e) {
     _remove_image_zoomer();
   }
 
-  function _zoom_image(e, options, image) {
+  function _zoom_image(e, options, image, temp_image_canvas) {
     var coordinates = _prepare_coordinates(e, options, image);
     $(zoom_canvas_name).css({left: coordinates.page_x, top: coordinates.page_y, "z-index": 99});
-    zoom_context.drawImage(image.get(0), coordinates.image_x, coordinates.image_y, options.width, options.height, 0, 0, options.width, options.height);
+    zoom_context.drawImage(temp_image_canvas.get(0), coordinates.image_x, coordinates.image_y, options.width, options.height, 0, 0, options.width, options.height);
   }
 
   function _prepare_coordinates(e, options, image) {
@@ -63,9 +64,19 @@
     return page_coodinates;
   }
 
+  function _insert_temp_canvas(zoom_canvas_container, image) {
+    var width = image.width(), height = image.height(), temp_image_canvas;
+    zoom_canvas_container.append("<canvas id='temp_image_canvas' width='" + width + "' height='" + height + "' style='position: absolute; left: -5000px;'></canvas>");
+    temp_image_canvas = $("#temp_image_canvas");
+    temp_image_canvas_context = temp_image_canvas.get(0).getContext("2d");
+    temp_image_canvas_context.drawImage(image.get(0), 0, 0, width, height);
+    return temp_image_canvas;
+  }
+
   function _remove_image_zoomer() {
     $(zoom_canvas_name).remove();
     $(zoom_canvas_container_name).find("img").unwrap();
+    $("#temp_image_canvas").remove();
   }
 
 })(jQuery);
